01fa1c4c90dcdb925bf0c53aaf64cab9b6c5619b
1RAJASDEV1 <rajasdev@gmail.com>
2026-06-26 12:00:23 +0530

    two pointer approach done

--- Arrays/005-remove-duplicates.py
+++ Arrays/005-remove-duplicates.py
@@ -14,3 +14,21 @@ class Solution:
         for i in range(len(ans)):
             nums[i] = ans[i]
         return len(ans)
+    
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        n=len(nums)
+        i=0 
+        j=1
+
+        while j<n:
+            if nums[j]==nums[i]:
+                j=j+1
+            else:
+                nums[i+1]=nums[j]
+                i=i+1
+                j=j+1
+
+        return i+1
