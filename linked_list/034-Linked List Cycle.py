8f9d600ab4adadc52c762974a3b7f4c22982e2ba
1RAJASDEV1 <rajasdev@gmail.com>
2026-07-18 17:48:43 +0530

    started linked list

---
+++ linked_list/034-Linked List Cycle.py
@@ -0,0 +1,33 @@
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        seen = set()
+
+        while head:
+
+            if head in seen:
+                return True
+
+            seen.add(head)
+            head = head.next
+
+        return False
+
+#above is brute force method
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
